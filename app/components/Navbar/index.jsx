cb85b578b4ab6b451a8e835ddc6fe08b23e2e2b1
MridulTi <mridultiwari2002@gmail.com>
2023-10-30 20:41:24 +0530

    Added Navbar for all pages

--- app/components/Navbar/index.jsx
+++ app/components/Navbar/index.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 
 function Navbar(props) {
     return (
-        <div>
-            Navbar
-        </div>
+        <>
+        <nav className='place-items-center py-4 px-48 w-screen bg-white shadow-lg'>
+            <h1 className='font-semibold text-xl'>Vital-Ease</h1>
+        </nav>
+        </>
     );
 }
 
