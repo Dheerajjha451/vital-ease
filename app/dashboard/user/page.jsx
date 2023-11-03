4ee4247e34f2afd80255302d722ad613793744fc
Dheeraj Jha <dheerajjha451@gmail.com>
2023-11-03 11:56:42 +0530

    Date picker

--- app/dashboard/user/page.jsx
+++ app/dashboard/user/page.jsx
@@ -1,46 +1,53 @@
-import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar, faBell } from '@fortawesome/free-solid-svg-icons';
+import Card from '../../../components/Cards/card'; 
+import Medication from '../../../components/Cards/Medication';
 import DashNav from '../../../components/DashNav';
+import BloodStatus from '../../../components/Cards/Bloodstatus';
+import ToDoCalendar from '../../../components/Cards/Todo';
 
-function Page(props) {
+const Page = (props) => {
   const { userName } = props;
 
   return (
-    <div>
+    <div className="flex">
       <DashNav />
-      <div className="text-right">
+
+      {/* Right side with content */}
+      <div className="text-right flex-1 p-4">
         <p>{userName}</p>
-        <h2>Today</h2>
+        <h2 className="text-2xl font-semibold mb-4">Today</h2>
+
         <div className="grid grid-cols-3 gap-4">
-          {/* Medication Reminder */}
-          <div className="bg-gray-200 p-4 rounded">
-            <h3>Medication Reminder</h3>
-            {/* Add specific content for Medication Reminder */}
-          </div>
+          <Card title="Medication Reminder" hoverEffect shadowEffect>
+            {[...Array(5)].map((_, index) => (
+              <Medication
+                key={index}
+                name={`Medicine ${index + 1}`}
+                quantity="1 pill"
+                timing="Morning"
+                hoverEffect
+                shadowEffect
+              />
+            ))}
+          </Card>
 
           {/* Today's Routine */}
-          <div className="bg-gray-200 p-4 rounded">
-            <h3>Today's Routine</h3>
-            {/* Add specific content for Today's Routine */}
-          </div>
+          <Card title="Today's Routine" hoverEffect shadowEffect>
+           
+            <ToDoCalendar />
+          </Card>
 
           {/* Health Stats */}
-          <div className="bg-gray-200 p-4 rounded">
-            <h3>Health Stats</h3>
-            {/* Add specific content for Health Stats */}
-          </div>
+          <Card title="Health Stats" hoverEffect shadowEffect>
+            <BloodStatus />
+          </Card>
         </div>
 
-        {/* Your Body Status */}
-        <div className="grid grid-cols-5 gap-4 mt-4">
-          {/* Add images or content for each grid item */}
-          <div className="bg-gray-200 p-4 rounded">
-            {/* Add content for the first grid item */}
-          </div>
-          {/* Add more grid items as needed */}
+     
         </div>
       </div>
-    </div>
   );
-}
+};
 
 export default Page;
